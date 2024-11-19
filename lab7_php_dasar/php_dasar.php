7d2e742488b816085496c15c4f02c291624f74b4
rovacantika <rovacantika@gmail.com>
2024-11-20 06:21:16 +0700

    Add files via upload

---
+++ lab7_php_dasar/php_dasar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+        echo "Hello World";
+    ?>
+    <h2>Menggunakan Variable</h2>
+    <?php
+    $nim = "312310390";
+    $nama = 'Rova cantika putri';
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama";
+    ?>
+</body>
+</html>
